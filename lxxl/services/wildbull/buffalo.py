faef599a3aac931699a922ed1c356b169bb7a38e
Manu <e.tabard@gmail.com>
2012-09-13 05:25:58 +0200

    add X-WWW-Authenticate

--- lxxl/services/wildbull/buffalo.py
+++ lxxl/services/wildbull/buffalo.py
@@ -2,6 +2,19 @@ from lxxl.lib import router, output
 from lxxl.lib.app import Error, Controller
 import requests
 
+"""
+  set resp.http.Access-Control-Allow-Origin = req.http.origin;
+  set resp.http.Access-Control-Allow-Methods = "POST, GET, HEAD, DELETE";
+  set resp.http.Access-Control-Expose-Headers = "X-WWW-Authenticate, X-UID, Date, Server, Location";
+  set resp.http.Access-Control-Allow-Headers = "X-IID, Authorization, X-Requested-With, X-Signature, Content-Type, X-Gate-Origin";
+  set resp.http.Access-Control-Max-Age = 10;
+  // This is freaky dangerous debuging trixing
+  if(req.http.User-Agent == "Talk to the hand"){
+   set resp.http.Access-Control-Allow-Methods = resp.http.Access-Control-Allow-Methods + ", PUT, CONNECT, TRACE, OPTIONS, ROX-PURGE, WHATEVER";
+   set resp.http.Access-Control-Allow-Headers = resp.http.Access-Control-Allow-Headers + ", Accept, Range, Expect, Allow, User-Agent, Host";
+   set resp.http.Access-Control-Expose-Headers = resp.http.Access-Control-Expose-Headers + ", D-vcl";
+
+"""
 
 class Buffalo(router.Root):
     SESSION = requests.session()
@@ -26,12 +39,17 @@ class Buffalo(router.Root):
                     'http://localhost:8082',
                     req.path_qs
                 ), headers=sendHeaders)
+
             except:
                 output.error('Auth Backend fail', 503)
 
             if int(resp.status_code / 100) != 2:
                 for (k, v) in resp.headers.items():
-                    wildResponse.headers[k] = v
+                    wildResponse.headers[k.lower()] = v
+                
+                if 'X-Requested-With' in req.headers:
+                    wildResponse.headers['X-WWW-Authenticate'] = wildResponse.headers['www-authenticate']
+
                 output.success(resp.json, resp.status_code)
                 raise Error('break')
 
