85bbc373e4acd1ed354cf23b4ac16cf4404db12b
Manu <e.tabard@gmail.com>
2012-11-12 22:03:47 +0100

    clear other tests

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -22,45 +22,45 @@ class BasicTest(unittest.TestCase):
     admin_url = "http://localhost:8084/1.0/key/"
     api_url = "localhost:8081"
 
-    def test_0service_alive(self):
-        """0. Ping services"""
-        result = requests.get(self.ping_url)
-        self.assertEqual(result.status_code, 401)
+    # def test_0service_alive(self):
+    #     """0. Ping services"""
+    #     result = requests.get(self.ping_url)
+    #     self.assertEqual(result.status_code, 401)
 
-    def test_1create_app_key(self):
-        """1. Sign Api key - good secret"""
-        result = requests.post(self.admin_url, data={
-            'keyid': 'TEST',
-            'xxxadmin': True,
-            'hosts': '*',
-            'secret': 'TEST'
-        })
-        self.assertIn(result.status_code, [201, 403])
+    # def test_1create_app_key(self):
+    #     """1. Sign Api key - good secret"""
+    #     result = requests.post(self.admin_url, data={
+    #         'keyid': 'TEST',
+    #         'xxxadmin': True,
+    #         'hosts': '*',
+    #         'secret': 'TEST'
+    #     })
+    #     self.assertIn(result.status_code, [201, 403])
 
-    def test_2bad_sign_api_key(self):
-        """2. Sign Api key - bad secret"""
-        try:
-            api = Lxxl('TEST', 'TEST2', host=self.api_url)
-            res = api.users.authenticate()
-        except exceptions.HTTPRequestException as e:
-            self.assertEqual(e.response.json['code'], '10')
+    # def test_2bad_sign_api_key(self):
+    #     """2. Sign Api key - bad secret"""
+    #     try:
+    #         api = Lxxl('TEST', 'TEST2', host=self.api_url)
+    #         res = api.users.authenticate()
+    #     except exceptions.HTTPRequestException as e:
+    #         self.assertEqual(e.response.json['code'], '10')
 
-    def test_3good_sign_api_key(self):
-        """3. Authentication Anonymous - bad password"""
-        try:
-            api = Lxxl('TEST', 'TEST', host=self.api_url)
-            res = api.users.authenticate()
-        except exceptions.HTTPRequestException as e:
-            self.assertEqual(e.response.json['code'], '20')
+    # def test_3good_sign_api_key(self):
+    #     """3. Authentication Anonymous - bad password"""
+    #     try:
+    #         api = Lxxl('TEST', 'TEST', host=self.api_url)
+    #         res = api.users.authenticate()
+    #     except exceptions.HTTPRequestException as e:
+    #         self.assertEqual(e.response.json['code'], '20')
 
-    def test_4log_anonymous(self):
-        """4. Authentication Anonymous - good password"""
-        try:
-            api = Lxxl('TEST', 'TEST', host=self.api_url)
-            api.setCredentials('anonymous', '860b9dbbda6ee5f71ddf3b44e54c469e')
-            api.users.create()
-        except exceptions.HTTPRequestException as e:
-            self.assertEqual(e.response.status_code, 400)
+    # def test_4log_anonymous(self):
+    #     """4. Authentication Anonymous - good password"""
+    #     try:
+    #         api = Lxxl('TEST', 'TEST', host=self.api_url)
+    #         api.setCredentials('anonymous', '860b9dbbda6ee5f71ddf3b44e54c469e')
+    #         api.users.create()
+    #     except exceptions.HTTPRequestException as e:
+    #         self.assertEqual(e.response.status_code, 400)
 
     def test_5_create_account(self):
         """5. Create Account"""
@@ -84,14 +84,14 @@ class BasicTest(unittest.TestCase):
         except exceptions.HTTPRequestException as e:
             raise Exception(e.response)
 
-    def test_6_log_user(self):
-        """6. Login with the new user"""
-        try:
-            api = Lxxl('TEST', 'TEST', host=self.api_url)
-            api.setCredentials('void@webitup.fr', 'toto42')
-            resp = api.users.authenticate(forcepost=True)
-        except exceptions.HTTPRequestException as e:
-            raise Exception(e.response.json)
+    # def test_6_log_user(self):
+    #     """6. Login with the new user"""
+    #     try:
+    #         api = Lxxl('TEST', 'TEST', host=self.api_url)
+    #         api.setCredentials('void@webitup.fr', 'toto42')
+    #         resp = api.users.authenticate(forcepost=True)
+    #     except exceptions.HTTPRequestException as e:
+    #         raise Exception(e.response.json)
 
 
 if __name__ == '__main__':
