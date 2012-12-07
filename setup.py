ee088224488ef15e1fa98752e89b77162b10458d
Manu <e.tabard@gmail.com>
2012-12-07 16:28:40 +0100

    change PIL fork

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requires = [
 
 dep_links = [
     'https://github.com/webitup/py3k-mailsnake/zipball/master#egg=mailsnake',
-    'https://github.com/sloonz/pil-py3k/archive/master.zip#egg=pil'
+    'https://github.com/webitup/pil-py3k-mirror/archive/master.zip#egg=pil'
 ]
 
 setup(
