1cb2c20b85eed7ab969db07642ebd0c35736253f
Manu <e.tabard@gmail.com>
2013-01-21 22:21:13 +0100

    LOOPPPPP BACKKKKK

--- lxxl/services/common/gate.py
+++ lxxl/services/common/gate.py
@@ -7,7 +7,7 @@ class Gate(router.Root):
 
     def fetch(self, environ, params):
         try:
-            host = "https://api.education-et-numerique.fr"
+            host = "https://www.education-et-numerique.fr"
             path = "/jsboot.js/%s/gate.html" % (params['gate'])
             gate = requests.get(host + path, verify=False)
 
